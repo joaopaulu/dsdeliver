380752a123d211ba92fcc41530ed31c075490b08
João Paulo <jptick@gmail.com>
2021-01-04 16:14:17 -0300

    Create Domain Model

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/model/Order.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dsdeliver.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
+    private static final long seriaLVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToMany
+    @JoinTable(name="tb_order_product",
+        joinColumns = @JoinColumn(name="order_id"),
+        inverseJoinColumns = @JoinColumn(name="product_id"))
+    private Set<Product> products = new HashSet<>();
+}
