5f97193fbf8db46f1628db67ed163aa9e7e7f905
João Paulo <jptick@gmail.com>
2021-01-06 13:47:26 -0300

    Create Componente Home page

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -3,14 +3,12 @@ import { ReactComponent as Logo } from "./logo.svg";
 
 function Navbar() {
   return (
-    <>
-      <nav className="main-navbar">
-        <Logo />
-        <a href="home" className="logo-text">
-          DS Delivery
-        </a>
-      </nav>
-    </>
+    <nav className="main-navbar">
+      <Logo />
+      <a href="home" className="logo-text">
+        DS Delivery
+      </a>
+    </nav>
   );
 }
 
